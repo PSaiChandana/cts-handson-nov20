97f410133aed429312c734d3b4ff82c2e697c670
PSaiChandana <Psai.Chandana@cognizant.com>
2020-12-08 16:58:13 +0530

    client

---
+++ P Sai Chandana (839018)/Afternoon/Microservices/client app/src/test/java/com/example/clientapp/ClientAppTests.java
@@ -0,0 +1,13 @@
+package com.example.clientoneapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
